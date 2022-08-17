5e7754614d27cc4036f17d788677c06fabf98b3f
Andrey Rybakov <rybakov.ad@icloud.com>
2022-08-17 18:53:28 +0200

    Clear the ExchangeModel class
    All atributes that are not intended for the world are private now.
    All methods that are internal now starts with underline.
    Make generation of service dictionaries more clear. and simplier.
    Now it is only needed to modify self.__nmaes_to_read,
    self.__format_functions, self._update_attributes and define
    format function and corresponding public attribute to add
    new read functionality

--- rad_tools/tb2j_tools/file_logic.py
+++ rad_tools/tb2j_tools/file_logic.py
@@ -1,23 +1,22 @@
 from copy import deepcopy
 
-import numpy as np
-
 
 class ExchangeModel:
     """
     Class containing the whole information extracted from TB2J *.out
 
-    Keeping resolved data from the file 
+    Keeping resolved data from the file
     and provide a number of functions to process it.
 
     Attributes
     ----------
 
 
+
     Methods
     -------
     filter(distance=None, number=None, template=None, from_scratch=False)
-        Filter all present exchange parameters 
+        Filter all present exchange parameters
         with respect to the provided conditions
 
     """
@@ -31,12 +30,12 @@ class ExchangeModel:
         filename : str
             Path to the TB2J *.out file.
         """
-        self.headers_to_functions = {
-            'Cell (Angstrom):\n': self._read_cell,
-            'Atoms:  \n': self._read_atoms,
-            'Orbitals used in decomposition: \n': self._read_orb_decomposition,
-            'Exchange: \n': self._read_exchange
-        }
+
+        self.cell = []
+        self.atoms = {}
+        self.magnetic_atoms = []
+        self.data = []
+
         self.__major_sep = '=' * 90 + '\n'
         self.__minor_sep = '-' * 88 + '\n'
         self.__garbage = str.maketrans({'(': None,
@@ -45,23 +44,29 @@ class ExchangeModel:
                                         ']': None,
                                         ',': None,
                                         '\'': None})
-        self.cell = None
-        self.atoms = None
-        self.magnetic_atoms = []
-        self.list_for_sort = []
-        self.data = [None, None, None, None]
-        self.keys = {'iso': 0,
-                     'aniso': 1,
-                     'dmi': 2,
-                     'distance': 3}
-        self.names_to_read = {'iso': 'J_iso:',
-                              'aniso': 'J_ani:',
-                              'dmi': 'DMI:',
-                              'distance': self.__minor_sep}
-        self.format_functions = {'iso': self._format_iso,
-                                 'aniso': self._format_aniso,
-                                 'dmi': self._format_dmi,
-                                 'distance': self._format_distance}
+
+        self.headers_to_functions = {
+            'Cell (Angstrom):\n': self._read_cell,
+            'Atoms:  \n': self._read_atoms,
+            'Orbitals used in decomposition: \n': self._read_orb_decomposition,
+            'Exchange: \n': self._read_exchange
+        }
+
+        self.__list_for_sort = []
+        self.__index = {}
+        self.__names_to_read = {'iso': 'J_iso:',
+                                'aniso': 'J_ani:',
+                                'dmi': 'DMI:',
+                                'distance': self.__minor_sep}
+        self.__format_functions = {'iso': self._format_iso,
+                                   'aniso': self._format_aniso,
+                                   'dmi': self._format_dmi,
+                                   'distance': self._format_distance}
+        i = 0
+        for key in self.__names_to_read:
+            self.__index[key] = i
+            self.data.append({})
+            i += 1
         with open(filename, 'r') as file:
             self._file_data = file.readlines()
         i = 0
@@ -74,29 +79,25 @@ class ExchangeModel:
         self.__data_nofilter = deepcopy(self.data)
 
     def _update_attributes(self):
-        self.iso = self.data[self.keys['iso']]
-        self.aniso = self.data[self.keys['aniso']]
-        self.dmi = self.data[self.keys['dmi']]
-        self.distance = self.data[self.keys['distance']]
+        self.iso = self.data[self.__index['iso']]
+        self.aniso = self.data[self.__index['aniso']]
+        self.dmi = self.data[self.__index['dmi']]
+        self.distance = self.data[self.__index['distance']]
 
     def _reset_data(self):
         for d in range(0, len(self.data)):
-            self.data[d] = None
+            self.data[d] = {}
 
     def _read_cell(self, i):
         i += 1
-        self.cell = np.array(
-            [
-                list(map(float, self._file_data[i].split())),
-                list(map(float, self._file_data[i + 1].split())),
-                list(map(float, self._file_data[i + 2].split()))
-            ],
-            dtype=float
-        )
+        self.cell = [
+            list(map(float, self._file_data[i].split())),
+            list(map(float, self._file_data[i + 1].split())),
+            list(map(float, self._file_data[i + 2].split()))
+        ]
         return i + 3
 
     def _read_atoms(self, i):
-        self.atoms = {}
         i += 3
         while 'Total' not in self._file_data[i]:
             self.atoms[self._file_data[i].split()[0]] = list(
@@ -108,14 +109,12 @@ class ExchangeModel:
         self.orb_for_decomposition = {}
         i += 2
         while ':' in self._file_data[i]:
-            self.orb_for_decomposition[self._file_data[i].split()[0]] = \
+            self.orb_for_decomposition[self._file_data[i].split()[0]] =\
                 self._file_data[i].translate(self.__garbage).split()[2:]
             i += 1
         return i
 
     def _prepare_dicts(self, atom_1, atom_2, key):
-        if self.data[key] is None:
-            self.data[key] = {}
         if atom_1 not in self.data[key]:
             self.data[key][atom_1] = {}
         if atom_2 not in self.data[key][atom_1]:
@@ -150,25 +149,26 @@ class ExchangeModel:
         if atom_2 not in self.magnetic_atoms:
             self.magnetic_atoms.append(atom_2)
 
-        self.list_for_sort.append((atom_1, atom_2, R, distance))
+        self.__list_for_sort.append((atom_1, atom_2, R, distance))
         return atom_1, atom_2, R, distance
 
     def _read_exchange(self, i):
         i += 2
         while i < len(self._file_data):
-            for name in self.names_to_read:
-                if (self.names_to_read[name] and
-                        self.names_to_read[name] in self._file_data[i]):
+            for name in self.__names_to_read:
+                if (self.__names_to_read[name] and
+                        self.__names_to_read[name] in self._file_data[i]):
 
                     if name == 'distance':
-                        atom_1, atom_2, R, tmp = self.format_functions[name](i)
+                        atom_1, atom_2, R, tmp =\
+                            self.__format_functions[name](i)
                     else:
-                        tmp = self.format_functions[name](i)
+                        tmp = self.__format_functions[name](i)
 
                     self._prepare_dicts(atom_1,
                                         atom_2,
-                                        key=self.keys[name])
-                    self.data[self.keys[name]][atom_1][atom_2][R] = tmp
+                                        key=self.__index[name])
+                    self.data[self.__index[name]][atom_1][atom_2][R] = tmp
             i += 1
         return i
 
@@ -184,15 +184,16 @@ class ExchangeModel:
         # TODO
         if template is not None:
             pass
+
         if number is not None:
             tmp = deepcopy(self.data)
             self._reset_data()
             for d in range(0, len(tmp)):
                 if tmp[d] is not None:
-                    for i in range(0, min(number, len(self.list_for_sort))):
-                        atom_1 = self.list_for_sort[i][0]
-                        atom_2 = self.list_for_sort[i][1]
-                        R = self.list_for_sort[i][2]
+                    for i in range(0, min(number, len(self.__list_for_sort))):
+                        atom_1 = self.__list_for_sort[i][0]
+                        atom_2 = self.__list_for_sort[i][1]
+                        R = self.__list_for_sort[i][2]
                         self._prepare_dicts(atom_1,
                                             atom_2,
                                             key=d)
@@ -207,7 +208,7 @@ class ExchangeModel:
                         for atom_2 in tmp[d][atom_1]:
                             for R in tmp[d][atom_1][atom_2]:
                                 if tmp[
-                                    self.keys['distance']
+                                    self.__index['distance']
                                 ][atom_1][atom_2][R] > distance:
                                     del self.data[d][atom_1][atom_2][R]
                             if not self.data[d][atom_1][atom_2]:
