a7afebbe6cd3ceaa2e498ab7113a65f6933c874f
Andrey Rybakov <rybakov.ad@icloud.com>
2022-08-18 16:05:48 +0200

    Refractor style

--- rad_tools/utest/test_routines.py
+++ rad_tools/utest/test_routines.py
@@ -24,29 +24,30 @@ class TestTerminalColours:
         assert TerminalCoulours.OK == TerminalCoulours.GREEN
         assert TerminalCoulours.ERROR == TerminalCoulours.RED
 
-    def test_get_256_colours(self):
-        print("\n Please check that the following colour's table "
-              "looks beautifull:\n")
-        for i in range(0, 16):
-            for j in range(0, 16):
-                print(f'{get_256_colours(16 * i + j)}'
-                      f'{16 * i + j:3}'
-                      f'{TerminalCoulours.RESET}',
-                      end=' ')
-            print()
-        with pytest.raises(ValueError):
-            get_256_colours(345)
-        with pytest.raises(ValueError):
-            get_256_colours(256)
-        with pytest.raises(ValueError):
-            get_256_colours(34.5)
-        with pytest.raises(ValueError):
-            get_256_colours(-45)
-        with pytest.raises(ValueError):
-            get_256_colours('sadfasd')
-        assert get_256_colours(0) == '\033[38:5:0m'
-        assert get_256_colours(255) == '\033[38:5:255m'
-        assert get_256_colours(34) == '\033[38:5:34m'
+
+def test_get_256_colours(self):
+    print("\n Please check that the following colour's table "
+          "looks beautifull:\n")
+    for i in range(0, 16):
+        for j in range(0, 16):
+            print(f'{get_256_colours(16 * i + j)}'
+                  f'{16 * i + j:3}'
+                  f'{TerminalCoulours.RESET}',
+                  end=' ')
+        print()
+    with pytest.raises(ValueError):
+        get_256_colours(345)
+    with pytest.raises(ValueError):
+        get_256_colours(256)
+    with pytest.raises(ValueError):
+        get_256_colours(34.5)
+    with pytest.raises(ValueError):
+        get_256_colours(-45)
+    with pytest.raises(ValueError):
+        get_256_colours('sadfasd')
+    assert get_256_colours(0) == '\033[38:5:0m'
+    assert get_256_colours(255) == '\033[38:5:255m'
+    assert get_256_colours(34) == '\033[38:5:34m'
 
 
 def test_check_make():
@@ -55,4 +56,5 @@ def test_check_make():
     check_make('tmp')
     assert isdir('tmp') == True
     check_make('tmp')
+    assert isdir('tmp') == True
     rmdir('tmp')
