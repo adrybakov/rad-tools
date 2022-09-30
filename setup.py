3ccae889f3bf6015f9d9bb12852942f1d3d634ba
Andrey Rybakov <rybakov.ad@icloud.com>
2022-09-30 23:03:17 +0200

    Set long_description to be equal to README content

--- setup.py
+++ setup.py
@@ -3,26 +3,10 @@ from setuptools import setup, find_packages
 
 __version__ = "0.0.0.3"
 
-long_description = """
-Script Collection
-=================
-Various scripts which were useful during my PhD.
-
-Installation
-============
-``pip3 install rad-tools``
-
-Update : ``pip3 install rad-tools --upgrade``
-
-`tb2j_plotter.py <https://github.com/adrybakov/rad-tools/blob/master/doc/tb2j_plotter.rst>`_
-============================================================================================
-Script for visualisation of TB2J exchange.out file.
----------------------------------------------------
-
-Allows the user to visualise the exchange.out file. 
-Currently sorting by R vector, distance and template file is supported.
-
-"""
+with open('README.rst', 'r') as file:
+    long_description = ''
+    for line in file:
+        long_description += line
 
 setup(
     name='rad-tools',
