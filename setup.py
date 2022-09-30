49e2d25fc3994d818a456a7585bc9c0a793d5b31
Andrey Rybakov <rybakov.ad@icloud.com>
2022-09-30 20:10:54 +0200

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from importlib.metadata import entry_points
 from setuptools import setup, find_packages
 
-__version__ = "0.0.0.1"
+__version__ = "0.0.0.2"
 
 long_description = """
 Script Collection
