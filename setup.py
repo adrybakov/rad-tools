e02d650ef5b22698ed34bfb4654ca855fb7c3d7d
Andrey Rybakov <rybakov.ad@icloud.com>
2022-09-30 20:09:37 +0200

    Update long description

--- setup.py
+++ setup.py
@@ -3,7 +3,20 @@ from setuptools import setup, find_packages
 
 __version__ = "0.0.0.1"
 
-long_description = """Collection of scripts from my PhD."""
+long_description = """
+Script Collection
+=================
+Various scripts which were useful during my PhD.
+
+`tb2j_plotter.py <https://github.com/adrybakov/rad-tools/blob/master/doc/tb2j_plotter.rst>`_
+============================================================================================
+Script for visualisation of TB2J exchange.out file.
+---------------------------------------------------
+
+Allows the user to visualise the exchange.out file. 
+Currently sorting by R vector, distance and template file is supported.
+
+"""
 
 setup(
     name='rad-tools',
