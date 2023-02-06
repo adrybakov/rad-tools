622c1009acea5cc9bdacc6c19225d0e5f6b28d29
Andrey Rybakov <rybakov.ad@icloud.com>
2023-02-06 13:06:55 +0100

    Refractor Bond class

--- rad_tools/exchange/bond.py
+++ rad_tools/exchange/bond.py
@@ -4,30 +4,26 @@ class Bond:
     r"""
     Exchange bond.
 
-    If ``matrix`` is specified then ``iso``, ``aniso`` and ``dmi`` will 
-    be ignored and derived from ``matrix``. If ``matrix`` is not specified 
-    then it will be derived from ``iso``, ``aniso`` and ``dmi``.
+    If ``matrix`` is specified then ``iso``, ``aniso`` and ``dmi`` are 
+    ignored and derived from ``matrix``. If ``matrix`` is not specified 
+    then it is derived from ``iso``, ``aniso`` and ``dmi``. 
+    If nothing is provided exchange matrix is set to zero matrix.
 
     Parameters
     ----------
     iso : int or float, default None
-        Value of isotropic exchange parameter in meV.
+        Value of isotropic exchange parameter.
     aniso : 3 x 3 array, None
-        3 x 3 matrix of symmetric anisotropic exchange in meV.
+        3 x 3 matrix of symmetric anisotropic exchange.
     dmi : 3 x 1 array, None
-        Dzyaroshinsky-Moria interaction vector :math:`(D_x, D_y, D_z)` in meV.
+        Dzyaroshinsky-Moria interaction vector :math:`(D_x, D_y, D_z)`.
     matrix : 3 x 3 array, None
-        Exchange matrix in meV. If ``matrix`` is specified then ``iso`` ,
-        ``aniso`` and ``dmi`` will be ignored and derived from ``matrix`` .
-        If ``matrix`` is not specified then it will be derived from
-        ``iso`` , ``aniso`` and ``dmi`` .
+        Exchange matrix. 
 
     Attributes
     ----------
     matrix : 3 x 3 array of floats
-        Exchange matrix in meV. 
-
-        Matrix form 
+        Exchange matrix. 
 
         .. code-block:: python
 
@@ -35,13 +31,16 @@ class Bond:
              [J_yx, J_yy, J_yz],
              [J_zx, J_zy, J_zz]]
 
-    iso : float
-    aniso : 3 x 3 array of floats
-    dmi : 3 x 1 array of floats
     symm_matrix : 3 x 3 array of floats
     asymm_matrix : 3 x 3 array of floats
+    iso : float
+    iso_matrix : 3 x 3 array of floats
+    aniso : 3 x 3 array of floats
+    aniso_diagonal : 3 x 1 array of floats 
+    aniso_diagonal_matrix : 3 x 3 array of floats 
+    dmi : 3 x 1 array of floats
+    dmi_matrix : 3 x 3 array of floats
     dmi_module : float
-    dmi_vs_iso : float
     """
 
     def __init__(self,
@@ -67,17 +66,22 @@ class Bond:
 
     @matrix.setter
     def matrix(self, new_matrix):
-        if new_matrix is None:
+        if new_matrix is not None:
+            new_matrix = np.array(new_matrix, dtype=float)
+            if new_matrix.shape != (3, 3):
+                raise ValueError("Matrix shape has to be equal to (3, 3)")
+        else:
             new_matrix = np.zeros((3, 3), dtype=float)
-        new_matrix = np.array(new_matrix)
-        if new_matrix.shape != (3, 3):
-            raise ValueError("Matrix shape have to be equal to (3, 3)")
         self._matrix = new_matrix
 
     @property
     def symm_matrix(self):
         r"""
         Symmetric part of exchange matrix.
+
+        .. math:: 
+
+            J_symm = \dfrac{J + J^T}{2}
         """
 
         return (self.matrix + self.matrix.T) / 2
@@ -86,6 +90,10 @@ class Bond:
     def asymm_matrix(self):
         """
         Asymmetric part of exchange matrix.
+
+        .. math:: 
+
+            J_asymm = \dfrac{J - J^T}{2}
         """
 
         return (self.matrix - self.matrix.T) / 2
@@ -93,16 +101,7 @@ class Bond:
     @property
     def iso(self):
         r"""
-        Value of isotropic exchange parameter in meV. 
-        If ``iso`` is not specified then it will be 0.
-
-        Matrix form: 
-
-        .. code-block:: python
-
-            [[J, 0, 0],
-             [0, J, 0],
-             [0, 0, J]]
+        Value of isotropic exchange parameter.
 
         Derived from the exchange matrix (:math:`\mathbf{J}`) as 
 
@@ -114,19 +113,31 @@ class Bond:
     @iso.setter
     def iso(self, new_iso):
         if new_iso is not None:
+            # correction is correct (see matrix update)
             new_iso = new_iso - self.iso
         else:
             new_iso = -self.iso
-        self.matrix = (self.matrix +
-                       (new_iso) *
-                       np.identity(3, dtype=float))
+        self.matrix = (self.matrix + (new_iso) * np.identity(3, dtype=float))
+
+    @property
+    def iso_matrix(self):
+        r"""
+        Isotropic part of the exchange matrix.
+
+        Matrix form: 
+
+        .. code-block:: python
+
+            [[J, 0, 0],
+             [0, J, 0],
+             [0, 0, J]]
+        """
+        return self.iso * np.identity(3, dtype=float)
 
     @property
     def aniso(self):
         r"""
-        3 x 3 matrix of symmetric anisotropic exchange in meV. 
-
-        Matrix form: 
+        3 x 3 matrix of symmetric anisotropic exchange. 
 
         .. code-block:: python
 
@@ -139,7 +150,7 @@ class Bond:
         .. math::
             \mathbf{J}_{aniso} = \mathbf{J}_{symm} - \dfrac{1}{3}Tr(\mathbf{J}) \cdot \mathbf{I}
 
-        where :math:`\mathbf{I}` is an :math:`3\times3` identity matrix.
+        where :math:`\mathbf{I}` is a :math:`3\times3` identity matrix.
         """
         return self.symm_matrix - self.iso * np.identity(3, dtype=float)
 
@@ -150,31 +161,44 @@ class Bond:
             if new_aniso.shape != (3, 3):
                 raise ValueError("Aniso matrix shape have " +
                                  "to be equal to (3, 3)")
+            # correction is correct (see matrix update)
             new_aniso = new_aniso - self.aniso
         else:
-            new_aniso = -self.aniso
+            new_aniso = - self.aniso
         self.matrix = self.matrix + new_aniso
+
+    @property
+    def aniso_diagonal(self):
+        r"""
+        Diagonal part of the symmetric anisotropic exchange.
+
+        .. code-block:: python
+
+            [J_xx, J_yy, J_zz]
+        """
+        return np.diag(self.aniso)
+
+    @property
+    def aniso_diagonal_matrix(self):
+        r"""
+        Diagonal part of the symmetric anisotropic exchange matrix.
+
+        .. code-block:: python
+
+            [[J_xx, 0, 0],
+             [0, J_yy, 0],
+             [0, 0, J_zz]]
+        """
+        return np.diag(np.diag(self.aniso))
 
     @property
     def dmi(self):
         r"""
-        Dzyaroshinsky-Moria interaction vector (Dx, Dy, Dz) in meV. 
-
-        Vector form: 
+        Dzyaroshinsky-Moria interaction vector (Dx, Dy, Dz). 
 
         .. code-block:: python
 
             [D_x, D_y, D_z]
-
-        Matrix form: 
-
-        .. code-block:: python
-
-            [[0, D_z, -D_y],
-             [-D_z, 0, D_x],
-             [D_y, -D_x, 0]]
-
-        Derived from antisymmetric part of exchange matrix (:math:`\mathbf{J}`).
         """
         return np.array([self.asymm_matrix[1][2],
                          self.asymm_matrix[2][0],
@@ -198,23 +222,25 @@ class Bond:
         self.matrix = self.matrix + dmi_matrix
 
     @property
+    def dmi_matrix(self):
+        r"""
+        Assymetric part of the exchange matrix.
+
+        .. code-block:: python
+
+            [[0, D_z, -D_y],
+             [-D_z, 0, D_x],
+             [D_y, -D_x, 0]]
+        """
+        return self.asymm_matrix
+
+    @property
     def dmi_module(self):
         r"""
-        Length of the DMI vector in th e units of exchange interaction.
+        Length of the DMI vector in the units of exchange interaction.
         """
 
         return sqrt(np.sum(self.dmi**2))
-
-    @property
-    def dmi_vs_iso(self):
-        r"""
-        Relative strength of DMI.
-
-        .. math::
-
-            \dfrac{\vert\vec{D}\vert}{\vert J_{iso}\vert}
-        """
-        return abs(self.dmi_module/self.iso)
 
     def round(self, accuracy=4):
         r"""Round exchange values.
