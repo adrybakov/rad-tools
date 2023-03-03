f586a2515e5dc5347e9e76c0855826abd7ee108c
Andrey Rybakov <rybakov.ad@icloud.com>
2023-03-03 01:55:33 +0100

    docs bugfix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,6 +55,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+#pydata bugfix
+icon_links = []
+
 
 # -- Options for HTML output -------------------------------------------------
 
