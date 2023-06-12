12e0e2862912293a355835f506b5b1054d278950
Andrey Rybakov <rybakov.ad@icloud.com>
2023-06-12 21:12:06 +0200

    Magnetic dipole-dipole

--- radtools/crystal/properties.py
+++ radtools/crystal/properties.py
@@ -1,111 +1,142 @@
-from radtools.crystal.crystal import Crystal
-from radtools.crystal.atom import Atom
-
 import numpy as np
 
 from tqdm import tqdm
 from math import pi
 
-from multiprocessing import Pool
+__all__ = ["dipole_dipole_energy", "dipole_dipole_interaction"]
+
+CONSTANT = 1.25663706212 * 9.2740100783**2 * 6.241509074 / 1000 / 4 / pi
 
 
-def compute_chunk(data):
-    array, end_index = data
-    end_index = min(len(array), end_index)
-    energy = 0
-    for i in range(0, end_index):
-        m_i = array[i, 0]
-        r_i = array[i, 1]
-        r_ij = array[i + 1 :, 1] - r_i
-        r_ij_norm = np.linalg.norm(r_ij, axis=1)
-        r_ij_norm_5 = (r_ij.T / r_ij_norm**5).T
-        first_term = np.sum(
-            r_ij_norm_5.T * np.diag(array[i + 1 :, 0] @ r_ij.T),
-            axis=1,
-        )
-        second_term = np.sum((array[i + 1 :, 0].T) / r_ij_norm**3, axis=1)
-        energy += m_i @ (-3 * first_term + second_term)
-    return energy
-
-
-def dipole_dipole_energy(crystal: Crystal, na, nb, nc, nproc=1):
+def dipole_dipole_energy(magnetic_centres, progress_bar=True, normalize=True):
     r"""
-    Computes dipole-dipole energy.
+    Computes magnetic dipole-dipole energy.
 
-    Formula:
+    This function computes the magnetic dipole-dipole energy of the set of magnetic centres:
 
     .. math::
 
-        E = \sum_{i\nej}\left(3(\vec{m_i} \cdot \ver{r_ij})(\vec{m_j} \cdot \ver{r_ij}) - (\vec{m_i}\cdot\vec{m_j})\right)
+        E = \frac{\mu_0}{4\pi}\sum_{i > j}\frac{1}{\vert r_{ij}\vert^3}\left(3(\vec{m_i} \cdot \vec{r_{ij}})(\vec{m_j} \cdot \vec{r_{ij}}) - (\vec{m_i}\cdot\vec{m_j})\right)
+
+    Parameters
+    ----------
+    magnetic_centres: (N, 2, 3) |array_like|_
+        List of N magnetic centres.
+        First element along second axis is magnetic moment (in Bohr magnetons),
+        Second element along second axis if position (in Angstrom).
+    progress_bar : bool, default True
+        Whether to show progressbar.
+    normalize : bool, default True
+        Whether to normalize energy to the number of magnetic centres.
+
+    Returns
+    -------
+    energy : float
+        Dipole-dipole energy of the system of ``magnetic_centres``.
+        Normalized to the number of magnetic centres N.
+
+    See Also
+    --------
+    dipole_dipole_interaction : between two sets of magnetic centres.
     """
 
-    constant = 1.25663706212 * 9.2740100783**2 * 6.241509074 / 1000 / 4 / pi
-
-    translations = (
-        np.transpose(np.indices((na, nb, nc)), (1, 2, 3, 0)).reshape((na * nb * nc, 3))
-        @ crystal.cell
-    )
-    n_t = len(translations)
-
-    magnetic_atoms = []
-    for atom in crystal:
-        try:
-            tmp = atom.magmom
-            magnetic_atoms.append(atom)
-        except ValueError:
-            pass
-
-    n_mag = len(magnetic_atoms)
-    magnetic_centres = np.zeros((n_t * len(crystal), 2, 3), dtype=float)
-    for a_i, atom in enumerate(magnetic_atoms):
-        magnetic_centres[a_i * n_t : (a_i + 1) * n_t, 0] = np.tile(
-            atom.magmom, (n_t, 1)
-        )
-        magnetic_centres[a_i * n_t : (a_i + 1) * n_t, 1] = translations + atom.position
-
-    if nproc == 1 or len(magnetic_centres) // nproc == 0:
-        energy = 0
-        for i in tqdm(range(len(magnetic_centres) - 1)):
-            m_i = magnetic_centres[i, 0]
-            r_i = magnetic_centres[i, 1]
-            r_ij = magnetic_centres[i + 1 :, 1] - r_i
-            r_ij_norm = np.linalg.norm(r_ij, axis=1)
-            r_ij_norm_5 = (r_ij.T / r_ij_norm**5).T
-            first_term = np.sum(
-                r_ij_norm_5.T * np.diag(magnetic_centres[i + 1 :, 0] @ r_ij.T),
-                axis=1,
-            )
-            second_term = np.sum(
-                (magnetic_centres[i + 1 :, 0].T) / r_ij_norm**3, axis=1
-            )
-            energy += (m_i @ (-3 * first_term + second_term)) / na / nb / nc / n_mag
+    energy = 0
+    if progress_bar:
+        for_range = tqdm(range(len(magnetic_centres) - 1))
     else:
-        pool_size = len(magnetic_centres) // nproc + 1
+        for_range = range(len(magnetic_centres) - 1)
 
-        with Pool(nproc) as p:
-            energy = (
-                np.sum(
-                    list(
-                        p.map(
-                            compute_chunk,
-                            [
-                                (magnetic_centres[i * pool_size :], pool_size)
-                                for i in range(nproc)
-                            ],
-                        )
-                    )
-                )
-                / na
-                / nb
-                / nc
-                / n_mag
-            )
+    N = len(magnetic_centres)
 
-    return energy * constant
+    for i in for_range:
+        m_i = magnetic_centres[i, 0]
+        r_i = magnetic_centres[i, 1]
+        r_ij = magnetic_centres[i + 1 :, 1] - r_i
+        r_ij_norm = np.linalg.norm(r_ij, axis=1)
+        r_ij_norm_5 = (r_ij.T / r_ij_norm**5).T
+        first_term = np.sum(
+            r_ij_norm_5.T * np.diag(magnetic_centres[i + 1 :, 0] @ r_ij.T),
+            axis=1,
+        )
+        second_term = np.sum((magnetic_centres[i + 1 :, 0].T) / r_ij_norm**3, axis=1)
+        energy += m_i @ (-3 * first_term + second_term)
+
+    if normalize:
+        energy /= N
+
+    return energy * CONSTANT
+
+
+def dipole_dipole_interaction(
+    magnetic_centres1, magnetic_centres2, progress_bar=True, normalize=True
+):
+    r"""
+    Computes magnetic dipole-dipole interaction.
+
+    This function computes dipole-dipole interaction between two sets of magnetic
+    centres (:math:`ms_1` and :math:`ms_2`):
+
+    .. math::
+
+        E = \frac{\mu_0}{4\pi}\sum_{i \in mc_1, j \in mc_2}\frac{1}{\vert r_{ij}\vert^3}\left(3(\vec{m_i} \cdot \vec{r_{ij}})(\vec{m_j} \cdot \vec{r_{ij}}) - (\vec{m_i}\cdot\vec{m_j})\right)
+
+    Parameters
+    ----------
+    magnetic_centres1: (N, 2, 3) |array_like|_
+        List of N magnetic centres of the first set.
+        First element along second axis is magnetic moment (in Bohr magnetons),
+        Second element along second axis if position (in Angstrom).
+    magnetic_centres2: (M, 2, 3) |array_like|_
+        List of M magnetic centres of the second set.
+        First element along second axis is magnetic moment (in Bohr magnetons),
+        Second element along second axis if position (in Angstrom).
+        Python cycle is running along this set.
+    progress_bar : bool, default True
+        Whether to show progressbar.
+    normalize : bool, default True
+        Whether to normalize energy to the number of magnetic centres.
+
+    Returns
+    -------
+    energy : float
+        Magnetic dipole-dipole interaction between ``magnetic_centres1`` and ``magnetic_centres2``.
+        Normalized to :math:`N\cdot M`.
+
+    See Also
+    --------
+    dipole_dipole_energy : within one set of magnetic centres.
+    """
+
+    energy = 0
+    if progress_bar:
+        for_range = tqdm(range(len(magnetic_centres2) - 1))
+    else:
+        for_range = range(len(magnetic_centres2) - 1)
+
+    NM = len(magnetic_centres1) + len(magnetic_centres2)
+
+    for i in for_range:
+        m_i = magnetic_centres2[i, 0]
+        r_i = magnetic_centres2[i, 1]
+        r_ij = magnetic_centres1[:, 1] - r_i
+        r_ij_norm = np.linalg.norm(r_ij, axis=1)
+        r_ij_norm_5 = (r_ij.T / r_ij_norm**5).T
+        first_term = np.sum(
+            r_ij_norm_5.T * np.diag(magnetic_centres1[:, 0] @ r_ij.T),
+            axis=1,
+        )
+        second_term = np.sum((magnetic_centres1[:, 0].T) / r_ij_norm**3, axis=1)
+        energy += m_i @ (-3 * first_term + second_term)
+
+    if normalize:
+        energy /= NM
+
+    return energy * CONSTANT
 
 
 if __name__ == "__main__":
     from radtools.crystal.atom import Atom
+    from radtools.crystal.crystal import Crystal
 
     crystal = Crystal()
     crystal.cell = [
@@ -123,28 +154,25 @@ if __name__ == "__main__":
         np.array((0.7500000000, 0.2500000000, 0.0737774367)) @ crystal.cell,
     )
 
-    print(Cr1.position, Cr2.position, sep="\n")
-
     crystal.add_atom(Cr1)
     crystal.add_atom(Cr2)
 
     crystal.Cr1.magmom = [0, 0, 3]
     crystal.Cr2.magmom = [0, 0, 3]
-    from time import time
 
-    start_time = time()
-    z = dipole_dipole_energy(crystal, 30, 30, 1)
-    print(z, time() - start_time, sep="\n")
-    start_time = time()
-    z = dipole_dipole_energy(crystal, 30, 30, 1, 72)
-    print(z, time() - start_time, sep="\n")
-    # z = dipole_dipole_energy(crystal, 10, 10, 1)
+    z10 = crystal.mag_dipdip_energy(10, 10, 1)
+    z15 = crystal.mag_dipdip_energy(15, 15, 1)
+    energies = crystal.converge_mag_dipdip_energy((10, 10, 1), (5, 5, 0), eps=0.1)
+    for e in energies:
+        print(f"{e[0]} ({e[1]})<------")
+
+    print(z10, z15, sep="\n")
     # crystal.Cr1.magmom = [0, 3, 0]
     # crystal.Cr2.magmom = [0, 3, 0]
-    # y = dipole_dipole_energy(crystal, 10, 10, 1)
+    # y = crystal.mag_dipdip_energy(10, 10, 1)
     # crystal.Cr1.magmom = [3, 0, 0]
     # crystal.Cr2.magmom = [3, 0, 0]
-    # x = dipole_dipole_energy(crystal, 10, 10, 1)
+    # x = crystal.mag_dipdip_energy(10, 10, 1)
 
     # print("x = ", x)
     # print("y = ", y)
