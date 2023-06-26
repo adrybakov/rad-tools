f79aee9bf4522934d536a5ac366130327f730050
Andrey Rybakov <rybakov.ad@icloud.com>
2023-06-26 21:27:50 +0200

    Prepare new examples for the run

---
+++ docs/examples/rad-plot-dos/collinear-spin-polarized/run.sh
@@ -0,0 +1,46 @@
+####################
+# Cluster settings #
+####################
+
+#!/bin/bash -l
+
+#Standard output and error:
+#SBATCH -o ./out.%j
+#SBATCH -e ./err.%j
+
+#Initial working directory:
+#SBATCH -D ./
+
+#Job Name
+#SBATCH -J pdos-csp
+
+#Number of nodes and MPI tasks per node:
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=72
+#SBATCH --cpus-per-task=1
+
+#Wall clock limit:
+#SBATCH --time=0-00:40:00
+
+#e-mail notifications
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=rybakov.ad@icloud.com
+
+####################
+# Load environment #
+####################
+
+# Make sure each process only uses one thread, that is, force VASP to use only one thread per core
+ulimit - s unlimited
+export OMP_NUM_THREADS =$SLURM_CPUS_PER_TASK
+# For pinning threads correctly:
+export OMP_PLACES = cores
+
+# Libraries and modules
+module load intel/21.3.0
+module load impi/2021.3
+module load qe/6.8
+
+srun pw.x -i input/scf.in > output/scf.out
+srun pw.x -i input/nscf.in > output/nscf.out
+srun projwfc.x -i input/projwfc.in > output/projwfc.out
