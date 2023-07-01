845e8cd2cbe95ea0b6c96eeae2d77aae10d612b0
Andrey Rybakov <rybakov.ad@icloud.com>
2023-07-01 16:28:25 +0200

    Work in progress

--- radtools/magnons/magnons.py
+++ radtools/magnons/magnons.py
@@ -3,7 +3,7 @@ from radtools.exchange.parameter import ExchangeParameter
 from radtools.routines import span_orthonormal_set
 from radtools.crystal.atom import Atom
 from radtools.magnons.diagonalization import solve_via_colpa, ColpaFailed
-from radtools.routines import print_2d_array, plot_horizontal_lines
+from radtools.routines import print_2d_array, plot_horizontal_lines, plot_vertical_lines
 
 from copy import deepcopy
 
@@ -58,6 +58,7 @@ class MagnonDispersion:
     """
 
     def __init__(self, model: ExchangeHamiltonian, Q=None, n=None):
+        self_omegas = None
         self._C = None
         # Store the exchange model, but privately
         self._model = deepcopy(model)
@@ -68,8 +69,6 @@ class MagnonDispersion:
             Q = [0, 0, 0]
         self.Q = np.array(Q, dtype=float) @ self._model.crystal.lattice.reciprocal_cell
 
-        print("Q: ", self.Q)
-
         # Convert n to absolute coordinates, use Q if n is not provided
         if n is None:
             if np.allclose([0, 0, 0], Q):
@@ -79,11 +78,8 @@ class MagnonDispersion:
         else:
             self.n = np.array(n, dtype=float) / np.linalg.norm(n)
 
-        print("n: ", self.n)
-
         # Get the number of magnetic atoms
         self.N = len(self._model.magnetic_atoms)
-        print("N: ", self.N)
 
         # Get the exchange parameters, indices and vectors form the ExchangeHamiltonian
         (
@@ -92,10 +88,6 @@ class MagnonDispersion:
             self.indices_j,
             self.dis_vectors,
         ) = self._model.input_for_magnons()
-
-        for i in range(0, len(self.J_matrices)):
-            print(self.indices_i[i], self.indices_j[i], self.dis_vectors[i])
-            print_2d_array(self.J_matrices[i], fmt=".4f")
 
         # Initialize spin vector, u vector and v vector arrays
         self.S = np.zeros((self.N, 3), dtype=float)
@@ -114,11 +106,14 @@ class MagnonDispersion:
                     f"Spin vector is not defined for {atom.fullname} atom."
                 )
 
+        for index in range(len(self.S)):
+            print(f"{index} : {self.S[index]}")
+            print(f"{index} : {self.u[index]}")
+            print(f"{index} : {self.v[index]}")
         # Rotate exchange matrices
         for i in range(len(self.J_matrices)):
             rotvec = self.n * (Q @ self.dis_vectors[i])
             R_nm = Rotation.from_rotvec(rotvec).as_matrix()
-            print_2d_array(R_nm)
             self.J_matrices[i] = self.J_matrices[i] @ R_nm
 
     def J(self, k):
@@ -138,11 +133,13 @@ class MagnonDispersion:
             In absolute coordinates.
         """
         # Initialize matrix
-        result = np.zeros((3, 3), dtype=complex)
+        result = np.zeros((self.N, self.N, 3, 3), dtype=complex)
 
         # Compute J(k)
         for index in range(len(self.J_matrices)):
-            result += self.J_matrices[index] * np.exp(
+            i = self.indices_i[index]
+            j = self.indices_j[index]
+            result[i][j] += self.J_matrices[index] * np.exp(
                 -1j * (k @ self.dis_vectors[index])
             )
 
@@ -168,14 +165,14 @@ class MagnonDispersion:
         result = np.zeros((self.N, self.N), dtype=complex)
 
         # Compute A(k)
-        for index in range(len(self.J)):
-            i = self.indices_i[index]
-            j = self.indices_j[index]
-            result[i][j] += (
-                sqrt(np.linalg.norm(self.S[i]) * np.linalg.norm(self.S[j]))
-                / 2
-                * (self.u[i] @ self.J(-k) @ np.conjugate(self.u[j]))
-            )
+        J = self.J(-k)
+        for i in range(len(J)):
+            for j in range(len(J[i])):
+                result[i][j] += (
+                    sqrt(np.linalg.norm(self.S[i]) * np.linalg.norm(self.S[j]))
+                    / 2
+                    * (self.u[i] @ J[i][j] @ np.conjugate(self.u[j]))
+                )
         return result
 
     def B(self, k):
@@ -198,14 +195,14 @@ class MagnonDispersion:
         result = np.zeros((self.N, self.N), dtype=complex)
 
         # Compute B(k)
-        for index in range(len(self.J_matrices)):
-            i = self.indices_i[index]
-            j = self.indices_j[index]
-            result[i][j] += (
-                sqrt(np.linalg.norm(self.S[i]) * np.linalg.norm(self.S[j]))
-                / 2
-                * (self.u[i] @ self.J(-k) @ self.u[j])
-            )
+        J = self.J(-k)
+        for i in range(len(J)):
+            for j in range(len(J[i])):
+                result[i][j] += (
+                    sqrt(np.linalg.norm(self.S[i]) * np.linalg.norm(self.S[j]))
+                    / 2
+                    * (self.u[i] @ J[i][j] @ self.u[j])
+                )
         return result
 
     def C(self):
@@ -222,15 +219,16 @@ class MagnonDispersion:
             self._C = np.zeros((self.N, self.N), dtype=complex)
 
             # Compute C matrix, note: sum over l is hidden here
-            for index in range(len(self.J_matrices)):
-                i = self.indices_i[index]
-                j = self.indices_j[index]
-                self._C[i, i] += (
-                    np.linalg.norm(self.S[j]) * self.v[i] @ self.J(0) @ self.v[j]
-                )
+            J = self.J(np.zeros(3))
+            for i in range(len(J)):
+                for j in range(len(J[i])):
+                    self._C[i][i] += (
+                        np.linalg.norm(self.S[j]) * self.v[i] @ J[i][j] @ self.v[j]
+                    )
         return self._C
 
     def h(self, k):
+        k = np.array(k)
         # Compute h matrix
         left = np.concatenate(
             (2 * self.A(k) - 2 * self.C(), 2 * np.conjugate(self.B(k)).T), axis=0
@@ -239,8 +237,6 @@ class MagnonDispersion:
             (2 * self.B(k), 2 * np.conjugate(self.A(-k)) - 2 * self.C()), axis=0
         )
         h = np.concatenate((left, right), axis=1)
-        print(k)
-        print_2d_array(h)
         return h
 
     # TODO Create compute() method and move all dispersion-related storage in the class. Make it iterable over omegas (not k points, but eigenvalues)
@@ -261,7 +257,6 @@ class MagnonDispersion:
         omegas : (N,) :numpy:`ndarray`
             Magnon energies for the vector ``k``.
         """
-
         # Diagonalize h matrix via Colpa
         try:
             omegas, U = solve_via_colpa(self.h(k))
@@ -274,6 +269,33 @@ class MagnonDispersion:
 
         return omegas
 
+    def omegas(self):
+        r"""
+        Dispersion spectra.
+        """
+
+        if self._omegas is None:
+            self.compute()
+
+        return self._omegas
+
+    def compute(self, kpoints, zeros_to_none=False):
+        r"""
+        Computes magnon energies with respect to the class settings.
+
+        Parameters
+        ----------
+        zeros_to_none : bool, default=False
+            If True, then return ``None`` instead of 0 if Colpa fails.
+        """
+
+        self._omegas = []
+
+        for point in kpoints:
+            self._omegas.append(self.omega(point, zeros_to_none=zeros_to_none))
+
+        self._omegas = np.array(self._omegas).T
+
 
 if __name__ == "__main__":
     from radtools.io.tb2j import read_tb2j_model
@@ -281,7 +303,6 @@ if __name__ == "__main__":
 
     model = read_tb2j_model(
         "/Users/rybakov.ad/Desktop/exchange.out",
-        quiet=False,
         bravais_type="HEX",
     )
     model.filter(max_distance=8)
@@ -290,8 +311,8 @@ if __name__ == "__main__":
     model.crystal.lattice.add_kpoint("Mprime", [0, 0.5, 0], "M$^{\prime}$")
     model.crystal.lattice.add_kpoint("Kprime", [-1 / 3, 2 / 3, 0], "K$^{\prime}$")
     kp = model.crystal.lattice.get_kpoints()  # Set custom k path
-    # kp.path = "G-K-M-G"
-    kp.path = "Mprime-G-M-K-Mprime-Kprime-G-K"
+    kp.path = "G-M-K-G"
+    # kp.path = "Mprime-G-M-K-Mprime-Kprime-G-K"
     kp.n = 40
 
     spin = ["Ni1", 0, 1, 0]
@@ -303,34 +324,79 @@ if __name__ == "__main__":
             atom.spin_vector = atom_spin
 
     # Get the magnon dispersion
-    dispersion = MagnonDispersion(model, Q=(0.138, 0, 0))
+    dispersion = MagnonDispersion(model, Q=(0.138, 0, 0), n=[0, 0, 1])
     dispersion2 = MagnonDispersion(model)
+
+    dispersion.compute(kp.points)
+    A = []
+    B = []
+    C = []
+    h = []
+
+    for point in kp.points:
+        A.append(dispersion.A(point))
+        B.append(dispersion.B(point))
+        C.append(dispersion.C())
+        h.append(dispersion.h(point))
+    h = np.array(h)
+
+    fig, ax = plt.subplots(15, 1, figsize=(5, 10))
+
+    fig.subplots_adjust(hspace=0)
+    ax[0].plot(kp.flatten_points, np.array(A).real[:, 0, 0], label="A real")
+    ax[1].plot(kp.flatten_points, np.array(A).imag[:, 0, 0], label="A imag")
+    ax[2].plot(kp.flatten_points, np.array(B).real[:, 0, 0], label="B real")
+    ax[3].plot(kp.flatten_points, np.array(B).imag[:, 0, 0], label="B imag")
+    ax[4].plot(kp.flatten_points, np.array(C).real[:, 0, 0], label="C real")
+    ax[5].plot(kp.flatten_points, np.array(C).imag[:, 0, 0], label="C imag")
+    ax[6].plot(kp.flatten_points, h.real[:, 0, 0], color="red", label="h 0 0 real")
+    ax[7].plot(kp.flatten_points, h.real[:, 0, 1], color="green", label="h 0 1 real")
+    ax[8].plot(kp.flatten_points, h.real[:, 1, 0], color="black", label="h 1 0 real")
+    ax[9].plot(kp.flatten_points, h.real[:, 1, 1], color="magenta", label="h 1 1 real")
+    ax[10].plot(kp.flatten_points, h.imag[:, 0, 0], color="red", label="h 0 0 imag")
+    ax[11].plot(kp.flatten_points, h.imag[:, 0, 1], color="green", label="h 0 1 imag")
+    ax[12].plot(kp.flatten_points, h.imag[:, 1, 0], color="black", label="h 1 0 imag")
+    ax[13].plot(kp.flatten_points, h.imag[:, 1, 1], color="magenta", label="h 1 1 imag")
+    ax[14].plot(
+        kp.flatten_points,
+        np.array(A).real[:, 0, 0] - np.array(B).real[:, 0, 0],
+        label="A - B real",
+    )
+    # ax[0].set_ylabel("A real")
+    # ax[1].set_ylabel("A imag")
+    # ax[2].set_ylabel("B real")
+    # ax[3].set_ylabel("B imag")
+    # ax[4].set_ylabel("C real")
+    # ax[5].set_ylabel("C imag")
+    # ax[6].set_ylabel()
+    # ax[7].set_ylabel()
+    # ax[8].set_ylabel()
+    # ax[9].set_ylabel()
+    # ax[10].set_ylabel()
+    # ax[11].set_ylabel()
+    # ax[12].set_ylabel()
+    # ax[13].set_ylabel()
+    for num, i in enumerate(ax):
+        i.set_xlim(kp.coordinates[0], kp.coordinates[-1])
+        i.set_xticks(kp.coordinates, kp.labels, fontsize=15)
+        plot_vertical_lines(i, kp.coordinates)
+        plot_horizontal_lines(i, 0)
+        i.legend(fontsize=8, loc="upper right")
+        if num != 14:
+            i.get_xaxis().set_visible(False)
+    plt.savefig("test.png", dpi=600, bbox_inches="tight")
+    plt.close()
 
     fig, ax = plt.subplots()
 
-    omegas = []
-    omegas_fm = []
-    for point in kp.points:
-        omegas.append(dispersion.omega(point, zeros_to_none=0))
-        omegas_fm.append(dispersion2.omega(point, zeros_to_none=0))
-
-    omegas = np.array(omegas).T
-    omegas_fm = np.array(omegas_fm).T
+    dispersion2.compute(kp.points)
 
     ax.set_xticks(kp.coordinates, kp.labels, fontsize=15)
     ax.set_ylabel("E, meV", fontsize=15)
-    ax.vlines(
-        kp.coordinates,
-        0,
-        1,
-        transform=ax.get_xaxis_transform(),
-        colors="black",
-        alpha=0.5,
-        lw=1,
-        ls="dashed",
-    )
-    ax.plot(kp.flatten_points, omegas[0], label="helix")
-    ax.plot(kp.flatten_points, omegas_fm[0], label="fm")
+
+    plot_vertical_lines(ax, kp.coordinates)
+    ax.plot(kp.flatten_points, dispersion.omegas()[0], label="helix")
+    ax.plot(kp.flatten_points, dispersion2.omegas()[0], label="fm")
     ax.legend()
 
     ax.set_xlim(kp.flatten_points[0], kp.flatten_points[-1])
