d92e9f07ee622d3af86dafbf0ad1506bfa10ad34
Andrey Rybakov <rybakov.ad@icloud.com>
2023-08-01 20:58:15 +0200

    Update tools

--- radtools/__init__.py
+++ radtools/__init__.py
@@ -7,7 +7,7 @@ All tools from the package.
 
 __version__ = "0.8.0.dev"
 __doclink__ = "rad-tools.org"
-__git_hash__ = "0a4321d52c146ec776b3664acb1ded38f1377c5b"
+__git_hash__ = "dbe22008559d5c11f8d0e9de16f5f9d52a47f781"
 __release_date__ = "1 August 2023"
 
 from . import (
--- tools/prepare-for-release.py
+++ tools/prepare-for-release.py
@@ -26,23 +26,32 @@ def check_active_branch(repo: git.Repo):
 
 def update_init(repo: git.Repo):
     print("Updating __init__.py...")
+
     cd = datetime.now()
     sha = repo.head.object.hexsha
+
+    # Read __init__.py
     init_file_path = join(ROOT_DIR, "radtools", "__init__.py")
     init_file = open(init_file_path, "r")
     init_file_content = init_file.readlines()
     init_file.close()
+
+    # Update __init__.py
     for l_i, line in enumerate(init_file_content):
+        # Update git hash
         if re.fullmatch("__git_hash__.*\n", line):
-            print("here")
             init_file_content[l_i] = f'__git_hash__ = "{sha}"\n'
+        # Update release date
         if re.fullmatch("__release_date__.*\n", line):
             init_file_content[
                 l_i
             ] = f'__release_date__ = "{cd.day} {month_name[cd.month]} {cd.year}"\n'
+
+    # Write __init__.py
     init_file = open(init_file_path, "w")
     init_file.writelines(init_file_content)
     init_file.close()
+
     print("Done")
 
 
