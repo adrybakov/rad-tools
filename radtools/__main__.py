43bbd41e250864eb8355942e4ca10d3db3a5aff0
Andrey Rybakov <rybakov.ad@icloud.com>
2023-09-20 01:47:08 +0200

    Update __main__

--- radtools/__main__.py
+++ radtools/__main__.py
@@ -1,18 +1,20 @@
 from argparse import ArgumentParser
 
-from radtools.decorate.stats import logo
+from radtools.decorate.stats import logo, license
 
 if __name__ == "__main__":
     parser = ArgumentParser(
         description="RAD-tools package",
     )
     parser.add_argument(
-        "--info",
+        "--license",
         action="store_true",
         default=False,
-        help="Prints information about the package.",
+        help="Prints the license of the package.",
     )
     args = parser.parse_args()
 
-    if args.info:
+    if args.license:
+        print(license())
+    else:
         print(logo())
