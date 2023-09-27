326f70743aa02977e94b99318303be737eb22cce
Andrey Rybakov <rybakov.ad@icloud.com>
2023-09-27 19:25:44 +0200

    Forgotten utf-8 addition

--- radtools/io/internal.py
+++ radtools/io/internal.py
@@ -1,18 +1,18 @@
 # RAD-tools - program for spin Hamiltonian and magnons.
 # Copyright (C) 2022-2023  Andrey Rybakov
-# 
+#
 # e-mail: anry@uv.es, web: adrybakov.com
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -393,7 +393,7 @@ def dump_spinham_txt(
     spinham_txt.append(main_separator)
     spinham_txt = "".join(spinham_txt)
     if filename is not None:
-        with open(filename, "w") as file:
+        with open(filename, "w", encoding="utf-8") as file:
             file.write(spinham_txt)
     else:
         print(spinham_txt)
