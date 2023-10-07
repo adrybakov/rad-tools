b007c2fc3b2fbc2d050679e8e000ed9d3b976863
Andrey Rybakov <rybakov.ad@icloud.com>
2023-10-07 15:16:42 +0200

    Update license metadata

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Chemistry",
         "Topic :: Scientific/Engineering :: Physics",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
     python_requires=">=3.8",
 )
