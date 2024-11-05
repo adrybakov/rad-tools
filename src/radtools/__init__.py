82a2d7e50dc8ee40deff804eac17a55dbb118e20
Andrey Rybakov <rybakov.ad@icloud.com>
2024-11-05 16:02:55 +0100

    Post-release commit

--- src/radtools/__init__.py
+++ src/radtools/__init__.py
@@ -23,7 +23,7 @@ RAD-tools
 
 __version__ = "0.9.1"
 __doclink__ = "rad-tools.org"
-__git_hash__ = "e2ed6f610024a1c2c2c3a5e8686bbe5c86196a17"
+__git_hash__ = "725251416f40fc6db48d04be42c6cd9ea25d4830"
 __release_date__ = "5 November 2024"
 
 from . import (
