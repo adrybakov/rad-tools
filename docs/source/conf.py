e4e25e009f2753bb3e301ee1807ea72c1d2e208d
Andrey Rybakov <rybakov.ad@icloud.com>
2025-10-20 12:35:53 +0200

    Update announcement message in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -120,7 +120,7 @@ else:
     switcher_version = f"{major}.{minor}"
     github_version = "stable"
 html_theme_options = {
-    "announcement": "Release of magnopy and wulfric  <a href='https://rad-tools.org/en/stable/announcement.html'>Read more here</a>",
+    "announcement": "Change of the domain  <a href='https://rad-tools.org/en/stable/announcement.html'>Read more here</a>",
     "collapse_navigation": True,
     "use_edit_page_button": True,
     "navbar_center": ["version-switcher", "navbar-nav"],
